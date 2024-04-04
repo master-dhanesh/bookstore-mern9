3e50aec5309376428b446cddf816f3c591977e91
master-dhanesh <dhanesh1296@gmail.com>
2024-04-04 20:05:15 +0530

    Update half created

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-const BOOKS = [];
+const BOOKS = [
+    {
+        name: "Book 1",
+        author: "Author 1",
+        price: 1234,
+        quantity: 10,
+        language: "Hindi",
+        category: "Fiction",
+        description:
+            "lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet",
+    },
+];
 
 router.get("/", function (req, res, next) {
     res.render("index");
@@ -23,6 +34,12 @@ router.get("/readall", function (req, res, next) {
 router.get("/delete/:index", function (req, res, next) {
     BOOKS.splice(req.params.index, 1);
     res.redirect("/readall");
+});
+
+router.get("/update/:index", function (req, res, next) {
+    const i = req.params.index;
+    const b = BOOKS[i];
+    res.render("update", { book: b, index: i });
 });
 
 module.exports = router;
