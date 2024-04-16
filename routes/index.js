139a1ba9951b1ab8884bd72f578d808f2a453ff9
master-dhanesh <dhanesh1296@gmail.com>
2024-04-16 20:32:14 +0530

    CRUD Mongo

--- routes/index.js
+++ routes/index.js
@@ -24,39 +24,70 @@ router.get("/create", function (req, res, next) {
     res.render("create");
 });
 
-router.post("/create", function (req, res, next) {
+router.post("/create", async function (req, res, next) {
+    try {
+        const newbook = new Books(req.body);
+        await newbook.save();
+        res.redirect("/readall");
+    } catch (error) {
+        res.send(error);
+    }
     // BOOKS.push(req.body);
     // res.redirect("/readall");
-    Books.create(req.body)
-        .then(() => {
-            res.redirect("/readall");
-        })
-        .catch((err) => res.send(err));
+    // Books.create(req.body)
+    //     .then(() => {
+    //         res.redirect("/readall");
+    //     })
+    //     .catch((err) => res.send(err));
 });
 
-router.get("/readall", function (req, res, next) {
-    Books.find()
-        .then((books) => {
-            res.render("library", { books: books });
-        })
-        .catch((err) => res.send(err));
+router.get("/readall", async function (req, res, next) {
+    try {
+        const books = await Books.find();
+        res.render("library", { books: books });
+    } catch (error) {
+        res.send(error);
+    }
+    // Books.find()
+    //     .then((books) => {
+    //         res.render("library", { books: books });
+    //     })
+    //     .catch((err) => res.send(err));
 });
 
-router.get("/delete/:index", function (req, res, next) {
-    BOOKS.splice(req.params.index, 1);
-    res.redirect("/readall");
+router.get("/delete/:id", async function (req, res, next) {
+    try {
+        await Books.findByIdAndDelete(req.params.id);
+        res.redirect("/readall");
+    } catch (error) {
+        res.send(error);
+    }
+    // BOOKS.splice(req.params.index, 1);
+    // res.redirect("/readall");
 });
 
-router.get("/update/:index", function (req, res, next) {
-    const i = req.params.index;
-    const b = BOOKS[i];
-    res.render("update", { book: b, index: i });
+router.get("/update/:id", async function (req, res, next) {
+    try {
+        const book = await Books.findById(req.params.id);
+        res.render("update", { book: book });
+    } catch (error) {
+        res.send(error);
+    }
+    // const i = req.params.index;
+    // const b = BOOKS[i];
+    // res.render("update", { book: b, index: i });
 });
 
-router.post("/update/:index", function (req, res, next) {
-    const i = req.params.index;
-    BOOKS[i] = req.body;
-    res.redirect("/readall");
+router.post("/update/:id", async function (req, res, next) {
+    try {
+        await Books.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect("/readall");
+    } catch (error) {
+        res.send(error);
+    }
+    // const i = req.params.index;
+    // BOOKS[i] = req.body;
+    // res.redirect("/readall");
 });
 
 module.exports = router;
